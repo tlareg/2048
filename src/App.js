ff05ffe8a3081309547ea3d29b000b880512d356
tlareg <tlaregaivir@gmail.com>
2019-03-15 22:06:03 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <div>App</div>
-      </div>
-    );
+const BOARD_SIZE = 4;
+const EMPTY_TILE = "EMPTY";
+
+const tileKey = (x, y) => `x${x}y${y}`;
+
+const initTiles = size => {
+  const tiles = {};
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      tiles[tileKey(x, y)] = EMPTY_TILE;
+    }
   }
-}
+  return tiles;
+};
+
+const setVal = ({ tiles, x, y, val }) => {
+  return { ...tiles, [tileKey(x, y)]: val };
+};
+
+const tileValToStr = val => (val === EMPTY_TILE ? "" : `${val}`);
+
+const Tile = ({ val }) => {
+  return <div className="tile">{tileValToStr(val)}</div>;
+};
+
+const Board = () => {
+  let tiles = initTiles(BOARD_SIZE);
+
+  tiles = setVal({ tiles, x: 1, y: 1, val: 1024 });
+
+  return (
+    <div className="board">
+      {Object.values(tiles).map(val => (
+        <Tile val={val} />
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <div>2048</div>
+      <Board />
+    </div>
+  );
+};
 
 export default App;
