69a34b8d4ed7ac1bd0030603bef87ec9c2b8f9d6
tlareg <tlaregaivir@gmail.com>
2020-07-04 01:45:16 +0200

    gh-pages init

---
+++ precache-manifest.0ed2b6de100c2ae8b1e24e30148c5c49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76f0d6b8d443db0ef38a1eec15f7e4b",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "184c52b9b0688418b71f",
+    "url": "/2048/static/css/main.91da4aef.chunk.css"
+  },
+  {
+    "revision": "074e2b27b0fa29e18ecc",
+    "url": "/2048/static/js/2.3221856d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2048/static/js/2.3221856d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "184c52b9b0688418b71f",
+    "url": "/2048/static/js/main.38754264.chunk.js"
+  },
+  {
+    "revision": "c3297bc798c8c34112c4",
+    "url": "/2048/static/js/runtime-main.557b35d6.js"
+  }
+]);
